873fe0da3aa1265fc9d94a9f3143f6be9211b624
thiru2612 <150767830+thiru2612@users.noreply.github.com>
2024-08-12 12:27:36 +0530

    Create day 1PAH-1.jsx

---
+++ day 1/day 1PAH-1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function World()
+{
+    return(
+      <div>
+        <p>
+          This is Team Functional Component
+        </p>
+    </div>)
+}
+export default World;
