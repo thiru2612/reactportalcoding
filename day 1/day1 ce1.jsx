2fbb1e5d5404ab729c11c26bff94953b6d725417
thiru2612 <150767830+thiru2612@users.noreply.github.com>
2024-08-12 11:57:59 +0530

    Update and rename day1 ce1 to day1 ce1.jsx

---
+++ day 1/day1 ce1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const alert = () => 
+{
+  alert("Message from Javascript Alert!")
+  console.log("Message to developer")
+}
+function click button()
+{
+ return(<div>
+<h1>Let we see the output of JAVASCRIPT</h1>
+  <button onClick={alerter}>Submit</button>
+ </div>)
+}
+export default load;
